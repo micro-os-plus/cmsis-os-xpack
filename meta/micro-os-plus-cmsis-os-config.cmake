a4fc334cc1580622015f418c364d2b1fb763f243
Liviu Ionescu <ilg@livius.net>
2021-02-26 11:20:37 +0200

    cmake: add find_package() support

---
+++ meta/micro-os-plus-cmsis-os-config.cmake
@@ -0,0 +1,58 @@
+#
+# This file is part of the µOS++ distribution.
+#   (https://github.com/micro-os-plus)
+# Copyright (c) 2021 Liviu Ionescu
+#
+# This Source Code Form is subject to the terms of the MIT License.
+# If a copy of the license was not distributed with this file, it can
+# be obtained from https://opensource.org/licenses/MIT/.
+#
+# -----------------------------------------------------------------------------
+
+# https://cmake.org/cmake/help/v3.18/
+# https://cmake.org/cmake/help/v3.18/manual/cmake-packages.7.html#package-configuration-file
+
+if(micro-os-plus-cmsis-os-included)
+  return()
+endif()
+
+set(micro-os-plus-cmsis-os-included TRUE)
+
+message(STATUS "Including micro-os-plus-cmsis-os...")
+
+# -----------------------------------------------------------------------------
+# The current folder.
+
+get_filename_component(xpack_current_folder ${CMAKE_CURRENT_LIST_DIR} DIRECTORY)
+
+# -----------------------------------------------------------------------------
+
+if (NOT TARGET micro-os-plus-cmsis-os-interface)
+
+  add_library(micro-os-plus-cmsis-os-interface INTERFACE EXCLUDE_FROM_ALL)
+
+  # ---------------------------------------------------------------------------
+
+  target_sources(
+    micro-os-plus-cmsis-os-interface
+
+    INTERFACE
+      ${xpack_current_folder}/src/cmsis-os-c-wrapper.cpp
+  )
+
+  target_include_directories(
+    micro-os-plus-cmsis-os-interface
+
+    INTERFACE
+      ${xpack_current_folder}/include
+  )
+
+  # ---------------------------------------------------------------------------
+  # Aliases.
+
+  add_library(micro-os-plus::cmsis-os ALIAS micro-os-plus-cmsis-os-interface)
+  message(STATUS "micro-os-plus::cmsis-os")
+
+endif()
+
+# -----------------------------------------------------------------------------
