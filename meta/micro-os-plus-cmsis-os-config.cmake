da436a2dac60f377f2fb942d7fec42e4791778cf
Liviu Ionescu <ilg@livius.net>
2021-03-04 00:31:27 +0200

    cmake: add xpack_process_package_version()

--- meta/micro-os-plus-cmsis-os-config.cmake
+++ meta/micro-os-plus-cmsis-os-config.cmake
@@ -18,7 +18,7 @@ endif()
 
 set(micro-os-plus-cmsis-os-included TRUE)
 
-message(STATUS "Including micro-os-plus-cmsis-os...")
+message(STATUS "Processing xPack ${PACKAGE_JSON_NAME}@${PACKAGE_JSON_VERSION}...")
 
 # -----------------------------------------------------------------------------
 # The current folder.
